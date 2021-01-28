1c0657459e23d8d4d6cb6bbb50733b5576f89cae
dkjr1 <73570166+dkjr1@users.noreply.github.com>
2021-01-27 19:12:20 -0800

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,9 @@
+namespace fun {
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
+}
